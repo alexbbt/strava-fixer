66e56bdf22ca3cf34e9ae1f199ae3087600c951c
Alexander Bell-Towne <alexander@bell-towne.com>
2020-02-01 19:45:54 -0500

    remove font awesome

--- src/main.js
+++ src/main.js
@@ -1,17 +1,9 @@
 import Vue from 'vue';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 import App from './App';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
-
-
-library.add(faTrash);
-
-Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
