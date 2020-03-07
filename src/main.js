681af3b29f4cee8cfd8444986abb9f1030e6a57b
Alexander Bell-Towne <alexander@bell-towne.com>
2020-03-07 00:49:18 -0500

    sentry

--- src/main.js
+++ src/main.js
@@ -1,9 +1,21 @@
 import Vue from 'vue';
 
+import * as Sentry from '@sentry/browser';
+import * as Integrations from '@sentry/integrations';
+
 import App from './App';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
+
+Sentry.init({
+  dsn: 'https://200ff2abff6743159bdc1cc9a5d70686@sentry.io/3900152',
+  integrations: [new Integrations.Vue({
+    Vue,
+    attachProps: true,
+    logErrors: true,
+  })],
+});
 
 Vue.config.productionTip = false;
 
